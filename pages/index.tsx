740b1d8a6f085295a34afdb70e327881c5c4406b
Alisha (Jungah Sohn) <aliciasohn815@gmail.com>
2022-05-23 10:38:00 +0200

    profile card css

--- pages/index.tsx
+++ pages/index.tsx
@@ -24,7 +24,33 @@ const Home: NextPage = () => {
           Checkout
         </div>
       </div>
-      <div className="bg-white p-10 shadow-lg rounded-lg w-3/4 mx-auto"></div>
+      <div className="w-3/4 mx-auto">
+        <div className="bg-blue-500 text-white flex justify-between overflow-hidden p-6 rounded-2xl py-11">
+          <span className="text-2xl pb-20">Profile</span>
+          <span>cart</span>
+        </div>
+        <div className="bg-white rounded-3xl relative -top-20 px-8 py-5  shadow-lg">
+          <div className="relative -top-20 flex flex-col items-center">
+            <div className="h-24 w-24 rounded-full bg-yellow-400"></div>
+          </div>
+          <div className="relative -top-24">
+            <div className="flex justify-between">
+              <div className="flex flex-col items-center">
+                <span className="text-gray-500 text-sm">Orders</span>
+                <span className="text-xl font-semibold">340</span>
+              </div>
+              <div className="flex flex-col items-center">
+                <span className="text-gray-500 text-sm">Spent</span>
+                <span className="text-xl font-semibold">$2,310</span>
+              </div>
+            </div>
+            <div className="flex flex-col items-center -mt-5 -mb-10">
+              <span className="font-bold text-2xl">Tony Molloy</span>
+              <span className="mt-2 text-gray-500">New York, USA</span>
+            </div>
+          </div>
+        </div>
+      </div>
       <div className="bg-white p-10 shadow-lg rounded-lg w-3/4 mx-auto"></div>
       <div className="bg-white p-10 shadow-lg rounded-lg w-3/4 mx-auto"></div>
     </div>
